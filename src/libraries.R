d7e8f4f24349f5b053532ef4b4f228076424c5e3
Simon Morvan <S_MORVAN@biopterre.com>
2025-12-14 18:46:25 -0500

    Modified library install / load script

--- src/libraries.R
+++ src/libraries.R
@@ -1,8 +1,61 @@
-# Common libraries
+# Installs the libraries needed
 
-pacman::p_load(pacman, agricolae, colorspace, cowplot, dunn.test,EnhancedVolcano,
-               DESeq2, glue,ggdist, ggforce, ggh4x, ggpubr, ggtext, forcats, 
-               here, indicspecies, labdsv, MASS, MicEco, pairwiseAdonis, phyloseq, psych, tidyverse,
-               roperators, vegan)
+#Installs pacman if needed
+if (!requireNamespace("pacman", quietly = TRUE)) install.packages("pacman")
+
+# Load  pacman
+library(pacman)
+
+# Forces the CRAN officiel CRAN mirror
+options(repos = c(CRAN = "https://cloud.r-project.org"))
+
+# --------
+# CRAN packages
+# --------
+cran_pkgs <- c("agricolae", "colorspace", "cowplot", "dunn.test",
+               "glue", "ggdist", "ggforce", "ggh4x",
+               "ggpubr", "ggtext", "forcats", "here", "labdsv",
+               "MASS","psych", "tidyverse", "roperators", "vegan")
+
+
+# Install and load CRAN packages
+pacman::p_load(char = cran_pkgs, install = TRUE)
+
+
+# --------
+# Bioconductor packages
+# --------
+bioc_pkgs <- c("DESeq2", "BiocGenerics","phyloseq","EnhancedVolcano",
+               "MicEco","pairwiseAdonis")  # BiocGenerics est souvent requis par DESeq2
+
+# Install and load  Bioconductor packages
+for (pkg in bioc_pkgs) {
+    if (!requireNamespace(pkg, quietly = TRUE)) {
+        if (!requireNamespace("BiocManager", quietly = TRUE)) {
+            install.packages("BiocManager")
+        }
+        BiocManager::install(pkg, update = FALSE, ask = FALSE)
+    }
+    library(pkg, character.only = TRUE)
+}
+
+# --------
+# GitHub packages
+# --------
+
+if (!requireNamespace("devtools", quietly = TRUE)) install.packages("devtools")
+library(devtools)
+
+# MicEco
+if (!requireNamespace("MicEco", quietly = TRUE)) {
+    devtools::install_github("Russel88/MicEco")
+}
+library(MicEco)
+
+# pairwiseAdonis
+if (!requireNamespace("pairwiseAdonis", quietly = TRUE)) {
+    devtools::install_github("pmartinezarbizu/pairwiseAdonis/pairwiseAdonis")
+}
+library(pairwiseAdonis)
 
 
